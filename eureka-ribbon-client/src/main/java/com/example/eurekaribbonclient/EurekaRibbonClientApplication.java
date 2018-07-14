401e7c445cea0d3b359d7afc7b730f650acde4bd
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2018-07-14 19:22:29 +0800

    add hystrix-dashboard into ribbon-client

--- eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/EurekaRibbonClientApplication.java
+++ eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/EurekaRibbonClientApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class EurekaRibbonClientApplication {
 

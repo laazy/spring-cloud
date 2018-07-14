1864821460281b9a555d312e24977dff4cfe63c3
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2018-07-14 18:49:32 +0800

    add hystrix into eureka-feign-client

--- eureka-feign-client/src/main/java/com/example/eurekafeignclient/HiService.java
+++ eureka-feign-client/src/main/java/com/example/eurekafeignclient/HiService.java
@@ -1,12 +1,14 @@
 package com.example.eurekafeignclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HiService {
     private final EurekaClientFeign eurekaClientFeign;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     public HiService(EurekaClientFeign eurekaClientFeign) {
         this.eurekaClientFeign = eurekaClientFeign;

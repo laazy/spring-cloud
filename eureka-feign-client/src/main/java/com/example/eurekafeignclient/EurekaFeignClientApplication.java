6a95973712968f2a5f84a0e9471f7abc715775e7
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2018-07-14 19:26:27 +0800

    add hystrix-dashboard into feign

--- eureka-feign-client/src/main/java/com/example/eurekafeignclient/EurekaFeignClientApplication.java
+++ eureka-feign-client/src/main/java/com/example/eurekafeignclient/EurekaFeignClientApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class EurekaFeignClientApplication {
 
     public static void main(String[] args) {

48a81ddf241b01a63d81773ceb34c050e5e747ed
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2018-07-29 12:17:02 +0800

    no message

--- admin-server/src/main/java/com/example/adminserver/SecurityConfig.java
+++ admin-server/src/main/java/com/example/adminserver/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().loginPage("/login.html")

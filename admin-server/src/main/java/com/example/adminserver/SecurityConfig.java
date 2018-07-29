7e3766d643fc66123c644ddb2c95ebd5d04dcd50
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2018-07-29 12:28:39 +0800

    修改security配置，使其使用security默认登录页面

--- admin-server/src/main/java/com/example/adminserver/SecurityConfig.java
+++ admin-server/src/main/java/com/example/adminserver/SecurityConfig.java
@@ -8,12 +8,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().loginPage("/login.html")
+        http.formLogin()
                 .loginProcessingUrl("/login").permitAll();
         http.logout().logoutUrl("/logout");
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/login.html", "/**/**/css", "/img/**", "third-party/**")
+                .antMatchers( "/**/**/css", "/img/**", "third-party/**")
                 .permitAll();
         http.authorizeRequests().antMatchers("/**").authenticated();
         http.httpBasic();
